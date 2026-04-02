785b9e124bfcbc4a2de419c9e1696ca8523560ac
Summerof69 <220586970+SO1213@users.noreply.github.com>
2026-04-02 09:34:57 -0400

    bug fix
    it would try to write the logs to ~/Downloads/Audit/ but the dir didn't exist.

--- audit.sh
+++ audit.sh
@@ -5,4 +5,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y -qq lynis
 #downloads ansi2html
 sudo DEBIAN_FRONTEND=noninteractive apt install -y -qq kbtin
 #runs a lynsis audit, logs and reports in the Audit folder, and the pipes it to ansi2html to generate an html report at Audit/lynis.html
+mkdir ~/Downloads/Audit
 sudo lynis audit system --logfile ~/Downloads/Audit/lynis.log --report-file ~/Downloads/Audit/lynis.dat | ansi2html -la > ~/Downloads/Audit/lynis.html
+echo "audit complete"
